02453aa830e44c9a80a22eca6c71993a8a86c303
rohanharikr <rohanharikumar80@gmail.com>
2022-04-12 16:05:07 +0400

    layout update

--- src/App.svelte
+++ src/App.svelte
@@ -6,7 +6,8 @@
 
 <main class="p-4 space-y-4">
   <section class="border border-charcoal w-full p-4 flex items-start space-x-12">
-    <div class="w-96">
+   
+    <div class="w-1/5">
       <h1 class="font-semibold">Authorization Server</h1>
       
       <ul class="space-y-2 mt-2">
@@ -21,19 +22,22 @@
       </ul>
 
       <div class="bg-gray-200 p-4 break-words my-6">
-        https://consent.hello.coop/
-        ?client_id=3574f001-0874-4b20-bffd-8f3e37634274
-        &redirect_uri=https://playground.hello.dev
-        &response_mode=fragment
-        &response_type=code
-        &nonce=28063994681536845045
-        &scope=email+picture+openid
+        <h2>Request URL</h2>
+        <span class="mt-2 block text-sm">
+          https://consent.hello.coop/
+          ?client_id=3574f001-0874-4b20-bffd-8f3e37634274
+          &redirect_uri=https://playground.hello.dev
+          &response_mode=fragment
+          &response_type=code
+          &nonce=28063994681536845045
+          &scope=email+picture+openid
+        </span>
       </div>
 
       <button class="hello-btn-dark">ō Continue with Hellō</button>
     </div>
 
-    <div class="w-96">
+    <div class="w-1/5">
       <h1 class="font-semibold">Scopes (* required)</h1>
       <div class="flex mt-2">
         <div class="w-1/2">
@@ -41,7 +45,7 @@
           <ul class="space-y-2 mt-2">
             <li class="flex items-center">
               <input type="checkbox" name="openid" id="openid">
-              <label for="openid" class="ml-2">openid</label>
+              <label for="openid" class="ml-2">openid *</label>
             </li>
             <li class="flex items-center">
               <input type="checkbox" name="" id="consent.hello.coop">
@@ -85,17 +89,88 @@
       </div>
     </div>
 
-    <div>
+    <div class="w-1/3">
       <h1 class="font-semibold">Query Params (* required)</h1>
       <div class="mt-2">
         <ul class="space-y-2 mt-2">
           <li class="flex items-center">
-            <input type="checkbox" name="client_id" id="client_id">
-            <label for="client_id" class="ml-2">client_id</label>
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">client_id *</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
           </li>
           <li class="flex items-center">
-            <input type="checkbox" name="redirect_uri" id="redirect_uri">
-            <label for="redirect_uri" class="ml-2">redirect_uri</label>
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">redirect_uri *</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">response_mode</label>
+            </div>
+            <div class="h-8 px-3 w-full border border-charcoal flex items-center">
+              <button class="bg-charcoal text-gray w-1/2">fragment</button>
+              <button class="w-1/2">query</button>
+            </div>
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">response_type</label>
+            </div>
+            <div class="h-8 px-3 w-full border border-charcoal flex items-center">
+              <button class="bg-charcoal text-gray w-1/2">code</button>
+              <button class="w-1/2">id_token</button>
+            </div>
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">code_challenge</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">code_verifier</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">prompt</label>
+            </div>
+            <div class="h-8 px-3 w-full border border-charcoal flex items-center">
+              <button class="bg-charcoal text-gray w-1/2">login</button>
+              <button class="w-1/2">profile_update</button>
+            </div>
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">nonce *</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">login_hint</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
+          </li>
+          <li class="flex items-center">
+            <div class="w-2/5 inline-flex items-center">
+              <input type="checkbox" name="client_id" id="client_id">
+              <label for="client_id" class="ml-2">state</label>
+            </div>
+            <input type="text" name="client_id" class="h-8 w-full">
           </li>
         </ul>
       </div>
@@ -120,3 +195,7 @@
     </button>
   </section>
 </main>
+
+<div class="fixed bottom-0 w-full">
+  <wc-footer/>
+</div>
