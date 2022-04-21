f1fa4f3dacb92d2e3d58b521f1c0bdda0035336f
rohanharikr <rohanharikumar80@gmail.com>
2022-04-21 16:24:21 +0400

    feat: add link to github new issue (#5)

--- src/App.svelte
+++ src/App.svelte
@@ -682,6 +682,26 @@
       </ul>
     {/if}
   </section>
+
+  <section class="py-6">
+    <a href="https://github.com/hellocoop/playground/issues/new" target="_blank" class="inline-flex items-center hover:underline">
+      <span>File an issue on GitHub</span>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          class="h-3 ml-1"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="2"
+            d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+          />
+        </svg>
+      </a>
+  </section>
 </main>
 
 <wc-footer/>
