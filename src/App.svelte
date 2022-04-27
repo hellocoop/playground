7cf6679231ba137f0d3c61ccbb5c6c7154e5760c
rohanharikr <rohanharikumar80@gmail.com>
2022-04-27 17:18:15 +0400

    ext link icon hello.dev

--- src/App.svelte
+++ src/App.svelte
@@ -330,8 +330,22 @@
         />
       </svg>
     </button>
-    <a href="https://hello.dev" target="_blank" class="w-1/3">
-      hello.dev
+    <a href="https://hello.dev" target="_blank" class="inline-flex items-center w-1/3">
+      <span>hello.dev</span>
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="h-4 ml-1 mt-0.5 opacity-80"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          stroke-width="2"
+          d="M10 6H6a2 2 0 00-2 2v10a2 2 0 002 2h10a2 2 0 002-2v-4M14 4h6m0 0v6m0-6L10 14"
+        />
+      </svg>
     </a>  
   </div>
   <span class="md:w-1/3 flex justify-center flex-shrink-0">
