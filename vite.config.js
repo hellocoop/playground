dc7c60ceee300b9335c1fe37881c18559ebc58dd
rohanharikr <rohanharikumar80@gmail.com>
2023-09-07 16:41:25 +0530

    change output dir

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,7 @@ import { svelte } from '@sveltejs/vite-plugin-svelte'
 export default defineConfig({
   plugins: [svelte()],
   build: {
-    outDir: 'docs',
+    outDir: 'S3',
     emptyOutDir: true
   }
 })
