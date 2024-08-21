23c02679dbc7835afb667d6775a12194fb585888
rohanharikr <rohanharikumar80@gmail.com>
2024-08-21 21:12:47 +0100

    switch between hello and public modes

--- src/App.svelte
+++ src/App.svelte
@@ -9,6 +9,7 @@
 	let readFromLocalStorage = false;
 	let darkMode = false;
 	let highlighter;
+	let isHelloMode = false;
 
 	const scopes = {
 		standard: [
@@ -22,25 +23,27 @@
 			'given_name',
 			'family_name'
 		],
-		// update: ['update_profile', 'update_email', 'update_phone', 'update_picture'],
+		pi_standard: ['preferred_username'],
 		custom: ['ethereum', 'discord', 'twitter', 'github', 'gitlab'],
-		required: ['openid'],
-		claims: [
-			'sub',
-			'name',
-			'nickname',
-			'given_name',
-			'family_name',
-			'email',
-			'phone',
-			'picture',
-			'ethereum',
-			'discord',
-			'twitter',
-			'github',
-			'gitlab'
-		]
+		pi_custom: [
+			// "mastodon",
+			// "instagram",
+			// "bio",
+			// "banner",
+			'recovery',
+			'verified_name',
+			'existing_name',
+			'existing_username'
+		],
+		required: ['openid']
 	};
+	scopes.claims = [
+		'sub',
+		...scopes.standard,
+		...scopes.pi_standard,
+		...scopes.custom,
+		...scopes.pi_custom
+	];
 
 	const possibleSlugs = [
 		'apple',
@@ -61,8 +64,8 @@
 		'ethereum',
 		'qrcode',
 		'passkey'
-		// 'managed'
 	];
+	const pi_possibleSlugs = ['managed'];
 
 	let invalidProviderHintSlug = null;
 	let debounceTimer;
@@ -75,7 +78,7 @@
 				const providerHintsArr = states.query_param_values?.provider_hint?.split(' ');
 				const invalidSlugs = providerHintsArr
 					.map((i) => i.replace('--', ''))
-					.filter((i) => !possibleSlugs.includes(i) && i);
+					.filter((i) => ![...possibleSlugs, ...(isHelloMode ?  pi_possibleSlugs : [])].includes(i) && i);
 
 				if (invalidSlugs?.length) {
 					invalidProviderHintSlug = Array.from(invalidSlugs);
@@ -105,34 +108,15 @@
 			const _states = JSON.stringify(states);
 			localStorage.setItem('states', _states);
 		}
+
+		isHelloMode = !!localStorage.plausible_ignore;
+
 		readFromLocalStorage = true;
 
 		processFragmentOrQuery();
 		updateFavicon();
 
 		sendEvent();
-
-		if (localStorage.plausible_ignore == 'true') {
-			const _standard_scopes = ['preferred_username'];
-			const _custom_scopes = [
-				// "twitter",
-				// "github",
-				// "gitlab",
-				// "mastodon",
-				// "instagram",
-				// "bio",
-				// "banner",
-				'recovery',
-				'verified_name',
-				'existing_name',
-				'existing_username'
-			];
-			scopes.standard = [...scopes.standard, ..._standard_scopes];
-			scopes.custom = [...scopes.custom, ..._custom_scopes];
-			scopes.claims = [...scopes.claims, ..._standard_scopes, ..._custom_scopes];
-			protocolParams.params.passkeys = states.protocol_param_values.passkeys = 'global';
-			queryParams.params.account = ['personal', 'managed'];
-		}
 
 		if (
 			!['https://wallet.hello.coop/authorize', ...states.custom_authorization_servers].includes(
@@ -181,6 +165,9 @@
 		params: {
 			provider_hint: ''
 		},
+		pi_params: {
+			account: ['personal', 'managed']
+		},
 		required: []
 	};
 	const protocolParams = {
@@ -197,6 +184,9 @@
 			login_hint: '',
 			scope: '',
 			custom: ''
+		},
+		pi_params: {
+			passkeys: 'global'
 		},
 		required: ['client_id', 'redirect_uri', 'nonce', 'response_type']
 	};
@@ -245,7 +235,8 @@
 			redirect_uri: window.location.origin + '/',
 			response_mode: 'fragment',
 			response_type: 'id_token',
-			prompt: ['consent']
+			prompt: ['consent'],
+			passkeys: 'global'
 		},
 		dropdowns: {
 			scopeParam: true,
@@ -704,7 +695,7 @@
 	});
 
 	async function sendEvent() {
-		if (localStorage.getItem('plausible_ignore') == 'true') {
+		if (isHelloMode) {
 			console.info('Ignoring Event: localStorage flag');
 			return;
 		}
@@ -798,9 +789,6 @@
 	</div>
 	<span class="md:w-1/3 flex justify-center flex-shrink-0">
 		<img src="logo.svg" alt="Hellō Playground" />
-		{#if localStorage.plausible_ignore}
-			<span class="absolute text-xs font-mono ml-60 -mt-1 text-green-500">devM0de</span>
-		{/if}
 	</span>
 	<div class="w-1/3 flex justify-end space-x-4">
 		<ul class="hidden lg:flex space-x-4">
@@ -909,18 +897,40 @@
 					>Authorization Request</span
 				>
 				{#if localStorage.plausible_ignore}
-					<button
-						on:click={() => {
-							localStorage.removeItem('plausible_ignore');
-							window.location.reload();
-						}}
-						class="absolute -top-3 right-24 bg-red-500 px-3 rounded-xl border border-charcoal dark:border-gray-800 text-sm bg-white dark:bg-[#151515]"
-						>Public</button
-					>
+					<div class="flex items-center absolute absolute -top-3.5 right-24">
+						<div>
+							<input
+								id="mode-hello"
+								value={true}
+								type="radio"
+								class="peer hidden"
+								bind:group={isHelloMode}
+							/>
+							<label
+								for="mode-hello"
+								class="cursor-pointer select-none rounded-l-full px-3 py-0.5 rounded-xl border-l border-y border-charcoal dark:border-gray-800 text-sm bg-white dark:bg-[#151515] peer-checked:bg-charcoal peer-checked:text-white"
+								>Hellō</label
+							>
+						</div>
+						<div>
+							<input
+								id="mode-public"
+								value={false}
+								type="radio"
+								class="peer hidden"
+								bind:group={isHelloMode}
+							/>
+							<label
+								for="mode-public"
+								class="cursor-pointer select-none rounded-r-full px-3 py-0.5 rounded-xl border border-charcoal dark:border-gray-800 text-sm bg-white dark:bg-[#151515] peer-checked:bg-charcoal peer-checked:text-white"
+								>Public</label
+							>
+						</div>
+					</div>
 				{/if}
 				<button
 					on:click={resetAll}
-					class="absolute -top-3 right-4 bg-red-500 px-3 rounded-xl border border-charcoal dark:border-gray-800 text-sm bg-white dark:bg-[#151515]"
+					class="absolute -top-3 right-4 px-3 rounded-xl border border-charcoal dark:border-gray-800 text-sm bg-white dark:bg-[#151515]"
 					>Reset</button
 				>
 
@@ -958,7 +968,7 @@
 							<div class="mt-2" transition:slide|local>
 								<div class="flex gap-x-4 pl-1">
 									<ul class="space-y-2 mt-2 w-44">
-										{#each scopes.standard as scope}
+										{#each scopes.standard.concat(isHelloMode ? scopes.pi_standard : []) as scope}
 											{@const required = scopes.required.includes(scope)}
 											<li
 												class="flex items-center"
@@ -980,7 +990,7 @@
 										{/each}
 									</ul>
 									<ul class="space-y-2 mt-2 truncate">
-										{#each scopes.custom as scope}
+										{#each scopes.custom.concat(isHelloMode ? scopes.pi_custom : []) as scope}
 											<li class="flex items-center truncate pl-1">
 												<input
 													type="checkbox"
@@ -1042,7 +1052,7 @@
 						</button>
 						{#if states.dropdowns.protocolParams}
 							<ul class="space-y-2 mt-2" transition:slide|local>
-								{#each Object.entries(protocolParams.params) as [param, value]}
+								{#each Object.entries( { ...protocolParams.params, ...(isHelloMode ? protocolParams.pi_params : {}) } ) as [param, value]}
 									{@const required = protocolParams.required.includes(param)}
 									{#if param === 'custom'}
 										<span class="pt-0.5 block" />
@@ -1192,7 +1202,7 @@
 						</button>
 						{#if states.dropdowns.queryParams}
 							<ul class="space-y-2 mt-2" transition:slide|local>
-								{#each Object.entries(queryParams.params) as [param, value]}
+								{#each Object.entries( { ...queryParams.params, ...(isHelloMode ? queryParams.pi_params : {}) } ) as [param, value]}
 									{@const required = queryParams.required.includes(param)}
 									<li
 										class="flex {param === 'provider_hint'
@@ -1293,7 +1303,7 @@
 													{/if}
 													<p class="text-xs mt-1.5">
 														<span class="opacity-80"
-															>{possibleSlugs
+															>{possibleSlugs.concat(isHelloMode ? pi_possibleSlugs : [])
 																.filter((i) => !['google', 'email', 'passkey'].includes(i))
 																.join(' ')}</span
 														><br />
